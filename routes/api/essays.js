143f52cf162f3ab13eb2955be6f93318b80e54c3
Mark Swan <ms.freemind@gmail.com>
2021-03-31 14:04:23 -0600

    Removed PUT route from essays

--- routes/api/essays.js
+++ routes/api/essays.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const passport = require('passport');
 
 const Essay = require('../../models/Essay');
-const Review = require('../../models/Review');
 const validateEssayInput = require('../../validation/essays');
 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -39,22 +38,6 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   });
 
   newEssay.save().then(essay => res.json(essay));
-});
-
-router.put('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const { errors, isValid } = validateEssayInput(req.body);
-
-  if (!isValid) return res.status(400).json(errors);
-
-  Essay.findOne({ _id: req.params.id, author: req.user })
-    .then(essay => {
-      essay.subject = req.body.subject;
-      essay.theme = req.body.theme;
-      essay.body = req.body.body;
-
-      essay.save().then(essay => res.json(essay));
-    })
-    .catch(err => res.status(404).json({ noessayfound: 'No essay found with that ID' }));
 });
 
 router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
