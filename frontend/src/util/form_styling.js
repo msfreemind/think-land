adecc0cfdcc2d38c0da5d8a845b86f15c43ec451
Mark Swan <ms.freemind@gmail.com>
2021-04-06 13:37:48 -0600

    Fixed bug in which footer was not staying at the bottom of the page when the text editor window became expanded.

--- frontend/src/util/form_styling.js
+++ frontend/src/util/form_styling.js
@@ -3,7 +3,6 @@ export const enableFormStyling = () => {
   document.getElementsByClassName("main")[0].style.width = "100%"; 
 
   document.getElementsByTagName("html")[0].style.height = "100%";
-  document.getElementsByTagName("html")[0].style.paddingBottom = "0";
   document.getElementsByTagName("html")[0].style.overflow = "hidden";
   document.getElementsByTagName("body")[0].style.height = "100%";
   document.getElementsByClassName("top-main")[0].style.height = "100%";
@@ -15,9 +14,8 @@ export const disableFormStyling = () => {
   document.getElementsByClassName("main")[0].style.width = "1250px";
   
   document.getElementsByTagName("html")[0].style.height = "";
-  document.getElementsByTagName("html")[0].style.paddingBottom = "20px";
   document.getElementsByTagName("html")[0].style.overflowY = "scroll";
   document.getElementsByTagName("body")[0].style.height = "";
-  document.getElementsByClassName("top-main")[0].style.height = "";
+  document.getElementsByClassName("top-main")[0].style.minHeight = "100vh";
   document.getElementById("root").style.height = "";
 }
