29a3a876f64b1ad28042bb53d315704436c58e91
Mark Swan <ms.freemind@gmail.com>
2021-04-06 18:32:57 -0600

    Configuring app for deployment to Heroku

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const drafts = require("./routes/api/drafts");
 const reviews = require("./routes/api/reviews");
 const categories = require("./routes/api/categories");
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 
@@ -25,6 +26,13 @@ app.use("/api/essays", essays);
 app.use("/api/drafts", drafts);
 app.use("/api/reviews", reviews);
 app.use("/api/categories", categories);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
