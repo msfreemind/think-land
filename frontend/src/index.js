2ac9f826c8721379daab06166bea18452787b7d9
Mark Swan <ms.freemind@gmail.com>
2021-04-08 21:18:00 -0600

    Removed the store from the window

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -36,8 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore({});
   }
 
-  window.store = store;
-
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
